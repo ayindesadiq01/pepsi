ebcd6bbcadd0498f3a7af3b45139c83e12da7b60
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-03-14 17:29:28 +0100

    new changes

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const buttonLeft = document.querySelector('.btn-left');
 const buttonCenter = document.querySelector('.btn-center');
 const buttonRight = document.querySelector('.btn-right');
-const bGround = document.querySelector('body');
+const bGround = document.querySelector('.container');
 const centerImage = document.querySelector('.center-img')
 const thirdImage = document.querySelector('.b-img3')
+const texting = document.querySelector('.texting')
 
 //  thirdImage.style.width = '200px'
 
 buttonLeft.addEventListener('click', function() {
- bGround.style.backgroundColor = 'blue';
-    centerImage.src = 'Pepsi_MAX_hero.png';
+ bGround.style.backgroundColor = 'rgb(41, 41, 150)';
+centerImage.src = 'Pepsi_MAX_hero.png';
 })
 buttonCenter.addEventListener('click', function() {
  bGround.style.backgroundColor = 'black';
-   centerImage.src = 'black_pepsi-removebg-preview.png';
+  centerImage.src = 'black_pepsi-removebg-preview.png';
 })
 buttonRight.addEventListener('click', function() {
  bGround.style.backgroundColor = 'red';
-  centerImage.src = 'sweat-removebg-preview.png';
+  centerImage.src = 'blue_pepsi-removebg-preview.png';
 })
